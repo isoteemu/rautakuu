729d895fe8cb898c18f1830f658270f1ed41bfd1
teemu <teemu@224c4803-7789-224a-91a8-0aefc35a1b09>
2005-02-21 16:18:57 +0000

    Importattu spesin sivut versiohallintaan

---
+++ config.inc.php
@@ -0,0 +1,5 @@
+<?php
+if( basename($_SERVER['SCRIPT_FILENAME']) != "index.php") die("ACCESS DENIED. COPS ARE COMING, PLEASE STANBY");
+
+$settings['horde']['db'] = "mysql://horde:aa48b0963481@localhost/horde";
+?>

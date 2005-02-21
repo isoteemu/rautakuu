06ee6e957ece65a72e809555cba3db570e531ac2
teemu <teemu@224c4803-7789-224a-91a8-0aefc35a1b09>
2005-02-21 16:21:38 +0000

    committed to wrong place

--- config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-if( basename($_SERVER['SCRIPT_FILENAME']) != "index.php") die("ACCESS DENIED. COPS ARE COMING, PLEASE STANBY");
-
-$settings['horde']['db'] = "mysql://horde:aa48b0963481@localhost/horde";
-?>

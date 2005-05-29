a3af61bc1c92b6d94866047b793a0a3cfdc093a3
teemu <teemu@224c4803-7789-224a-91a8-0aefc35a1b09>
2005-05-29 17:24:35 +0000

    new approach. Will bring troubles

--- MiniTeemu/plugins/un1337.php
+++ MiniTeemu/plugins/un1337.php
@@ -6,23 +6,20 @@ if( $init==true ) {
 }
 
 $teinix = array(
-    '/[^\d\w]omg[^\d\w]/i',
-    '/[^\d\w]imac[^\d\w]/i',
-    '/[^\d\w]vinq[^\d\w]/i',
-    '/[^\d\w]parq[^\d\w]/i',
-    '/[^\d\w]lol[^\d\w]/i',
-    '/[^\d\w]itq[^\d\w]/i',
-    '/[^\d\w]stfu[^\d\w]/i',
+    'omg',
+    'imac',
+    'vinq',
+    'parq',
+    'lol',
+    'itq',
+    'stfu',
 );
 
-$tulitikut = array();
-
-if( preg_match_all( $teinix, $plugin->line->msg, $tulitikut )) {
-    for ($i=0; $i< count($tulitikut[0]); $i++) {
-        $sanat .= " {$matches[0][$i]}";
+foreach($teinix as $str) {
+    if ($pos = stristr($plugin->line->msg, $str)) {
+        irc::trace("Teinixiä havaittu:{$str} in {$plugin->line->msg}[pos:$pos]");
+        $plugin->send("KICK {$plugin->line->nick}: Teinixiä havaittu. Terveiset Chevyltä.");
     }
-    irc::trace("Teinixiä havaittu:{$sanat}");
-    $plugin->send("KICK {$plugin->line->nick}: Teinixiä havaittu. Terveiset Chevyltä.");
 }
 
 ?>

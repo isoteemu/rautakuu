4501f84f14f4ee18c4a5b7b7ad1d182e9b9d9d08
teemu <teemu@224c4803-7789-224a-91a8-0aefc35a1b09>
2005-06-03 23:22:58 +0000

    Use similar_text to check if Servo imitation was anywhere near

---
+++ MiniTeemu/plugins/servoBetoni.php
@@ -0,0 +1,16 @@
+<?php
+
+if( $init==true ) {
+    $plugin->addRule('code' => 'PRIVMSG');
+    return;
+}
+
+$key = "Mikä on kun ei taidot riitä, mikä on kun ei onnistu?";
+
+$percent = 0;
+similar_text($key, $plugin->line->msg, $percent);
+if( $percent >= 95 ) {
+    $plugin->irc->message('"Heikoille voi olla liikaa kutsu metallin, muttei niille joita ohjaa vaisto soturin!"');
+}
+
+?>

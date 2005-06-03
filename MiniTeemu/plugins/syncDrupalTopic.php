617e9fdbddaead04822ac4f0284daa6cd8ada33b
teemu <teemu@224c4803-7789-224a-91a8-0aefc35a1b09>
2005-06-03 23:23:36 +0000

    on topic change, update drupla topic

---
+++ MiniTeemu/plugins/syncDrupalTopic.php
@@ -0,0 +1,14 @@
+<?php
+if( $init==true ) {
+    $plugin->addRule('code', "TOPIC");
+
+    // DB connection
+    include_once("DB.php");
+    $db = DB::Connect("mysql://miniteemu:a24cdcf4903642@localhost/drupal");
+}
+
+$ser = serialize($plugin->line->msg);
+$db->query("UPDATE `variable` SET `value`='{$ser}' WHERE `name` = 'site_slogan'");
+
+
+?>

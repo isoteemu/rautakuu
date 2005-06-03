6b9dc0e0203f143561276d9aacf14b7e5285eecc
teemu <teemu@224c4803-7789-224a-91a8-0aefc35a1b09>
2005-06-03 23:40:37 +0000

    Pear error handlers

--- MiniTeemu/plugins/syncDrupalTopic.php
+++ MiniTeemu/plugins/syncDrupalTopic.php
@@ -8,10 +8,19 @@ if( $init==true ) {
     // DB connection
     include_once("DB.php");
     $db = DB::Connect("mysql://miniteemu:a24cdcf4903642@localhost/drupal");
+    if (PEAR::isError($db)) {
+        irc::trace("DB Error: ".$db->getMessage());
+    }
+    $db = false;
 }
 
-$ser = serialize($plugin->line->msg);
-$db->query("UPDATE `variable` SET `value`='{$ser}' WHERE `name` = 'site_slogan'");
+if( $db == false ) return;
 
+$ser = serialize($plugin->line->msg);
+$res =& $db->query("UPDATE `variable` SET `value`='{$ser}' WHERE `name` = 'site_slogan'");
+
+if( PEAR::isError($res) ) {
+    irc::trace("DB Error: ".$res->getMessage());
+}
 
 ?>

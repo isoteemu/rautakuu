3ce1bc4bcfe9c0f9c81749b63ae803b41e8ff832
teemu <teemu@224c4803-7789-224a-91a8-0aefc35a1b09>
2005-10-08 19:41:59 +0000

    Nazikytta plugin. Kicks people who are away and yet, changes theyre nick

---
+++ MiniTeemu/plugins/nazikytta.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * nazikytta.php -- tarkastaa onko käyttäjä away nickin vaihdon yhteydessä.
+ */
+if( $init==true ) {
+    $plugin->addRule('code', "NICK");
+    return;
+}
+$exp = time()+10;
+$toban = substr($plugin->line->from, strpos($plugin->line->from, "~")+1);
+// Luodaan uusi plugin.
+// TODO: vitun ääliömäistä. keksi jotain fiksumpaa
+
+$code = '
+if( $init==true ) {
+    $plugin->addRule("code", "301");
+    $plugin->addRule("nick", "'.$plugin->line->msg.'");
+    $plugin->addRule("expire", "'.$exp.'");
+    return;
+}
+
+//print_r($plugin->line);
+
+irc::trace("Away scripti havaittu '.$plugin->line->msg.'");
+//$plugin->irc->send("MODE '.$plugin->irc->channel.' +b *!*'.$toban.'");
+$plugin->irc->send("KICK '.$plugin->irc->channel.' '.$plugin->line->msg.' :http://rautakuu.org/drupal/RautakuuIrc #4");
+
+$plugin->expire();
+';
+
+// Rekisteröidään plugini uutena
+$plugin->irc->irc_data->triggers->newPlugin($code);
+
+// Lähetetään whois kysely
+$plugin->irc->send("WHOIS {$plugin->line->msg}");
+
+?>

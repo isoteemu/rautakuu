da88dbf06bf32a9fe8ee1c8f6502ab88cc3caa1b
teemu <teemu@224c4803-7789-224a-91a8-0aefc35a1b09>
2005-11-06 22:46:07 +0000

    Some more and less usefull bash scripts

---
+++ myth.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+[ -f $HOME/.Xdefaults ] && xrdb $HOME/.Xdefaults
+
+export LC_ALL="fi_FI@euro"
+
+xsetroot -solid black
+if [ -x /usr/lib/xscreensaver/glmatrix ]; then
+    (
+        /usr/lib/xscreensaver/glmatrix -root -delay 15000 &
+        xpid=$!
+        sleep 1
+        renice 19 $xpid
+    ) &
+else
+    xsetbg -fullscreen -onroot '/home/teemu/Documents/Pics/wallpapers/GTSAC 03.jpg' &
+fi
+
+xset s noblank &
+xset s off &
+xset -dpms &
+
+# Asetetaan meikäläisen näppis. (paitsi että lenti seinään ja meni paskaks :/)
+#if [ "$(hostname)" = "isoteemu" ]; then
+#    setxkbmap -model microsoftmult -layout fi -variant basic
+#fi
+
+
+if [ $(which evilwm 2> /dev/null) ]; then
+    evilwm -bw 0 &
+elif [ $(which metacity 2> /dev/null) ]; then
+    metacity &
+elif [ $(which kwin 2> /dev/null) ]; then
+    (
+        dcopserver && kwin
+    ) &
+fi
+
+if [ $(which xvattr 2> /dev/null) ]; then
+    #xvattr -a XV_COLORKEY -v 66048 &
+    xvattr -a XV_COLORKEY -v 1 &
+fi
+
+if [ $(which nvidia-settings 2> /dev/null) ]; then
+    nvidia-settings -l
+    nvidia-settings --assign="SyncToVBlank=1"
+    nvidia-settings --assign="DigitalVibrance=6"
+    nvidia-settings --assign="ImageSharpening=1.9"
+    nvidia-settings --assign="TVFlickerFilter=140"
+    nvidia-settings --assign="TVOverScan=12"
+fi
+
+unset http_proxy
+
+mythfrontend
